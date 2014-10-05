be6cc92b2aea30ee1649149959bd7cf032e2dd80
Tomáš Voslař <t.voslar@gmail.com>
2014-10-05 11:24:26 +0100

    Update SearchPresenter.php

--- Frontend/presenters/SearchPresenter.php
+++ Frontend/presenters/SearchPresenter.php
@@ -29,31 +29,31 @@ class SearchPresenter extends \FrontendModule\BasePresenter{
 
     }
 	
-    public function renderDefault($id){
-
+    public function renderDefault($id)
+    {
         $this->template->results = $this->results;
-	$this->template->id = $id;
+        $this->template->id = $id;
     }
     
-    public function createComponentSearchForm($name, $context = null, $fromPage = null){
-        
-	if($context != null){
-	    
-	    $form = new UI\Form();
+    public function createComponentSearchForm($name, $context = null, $fromPage = null)
+    {
+    	if($context != null){
+    	    
+    	    $form = new UI\Form();
 
-	    $form->getElementPrototype()->action = $context->link('default', array(
-		    'path' => $fromPage->getPath(),
-		    'abbr' => $context->abbr,
-		    'do' => 'searchForm-submit'
-	    ));
+    	    $form->getElementPrototype()->action = $context->link('default', array(
+    		    'path' => $fromPage->getPath(),
+    		    'abbr' => $context->abbr,
+    		    'do' => 'searchForm-submit'
+    	    ));
 
-	    $form->setTranslator($context->translator);
-	    $form->setRenderer(new BootstrapRenderer);
+    	    $form->setTranslator($context->translator);
+    	    $form->setRenderer(new BootstrapRenderer);
 
-	}else{
-	    $form = $this->createForm('searchForm-submit');
-	}
-	
+    	}else{
+    	    $form = $this->createForm('searchForm-submit');
+    	}
+
         $form->setMethod('get');
         
         $form->addText('q', 'Search phrase')->setDefaultValue($this->getParameter('q'))->setAttribute('class', array('form-control'));
@@ -64,37 +64,45 @@ class SearchPresenter extends \FrontendModule\BasePresenter{
         return $form;
     }
     
-    public function searchFormSubmitted($form){
+    public function searchFormSubmitted($form)
+    {
         $values = $form->getValues();
         
         $packages = $this->em->getRepository('WebCMS\SearchModule\Doctrine\SearchSetting')->findBySearch(true);
         
-	$results = array();
-	if(strlen($values->q) > 0){
-	
-	    // fetch pages where to search
-	    foreach($packages as $p){
-		$module = $this->createObject($p->getPackage());
-
-		if($module->isSearchable()){
-		    $tmp = $module->search($this->em, $values->q, $this->language);
-		    $results = array_merge($results, $tmp);
-		}
-	    }
-	
-	}else{
-	    $this->flashMessage('Nothing to search. Please type search phrase.', 'danger');
-	}
+		$results = array();
+		if(strlen($values->q) > 0) {
+		    // fetch pages where to search
+		    foreach($packages as $p){
+                $module = $this->createObject($p->getPackage());
+    			if($module->isSearchable()){
+    			    $tmp = $module->search($this->em, $values->q, $this->language);
+    			    $results = array_merge($results, $tmp);
+    			}
+    	    }
+    	} else {
+    	    $this->flashMessage('Nothing to search. Please type search phrase.', 'danger');
+    	}
         
-        $this->results = $results;        
+        if ($this->isAjax()) {
+            $arrayResult = array();
+            foreach ($results as $result) {
+                $arrayResult[] = $result->toArray();
+            }
+
+            $this->payload->results = $arrayResult;
+            $this->sendPayload();
+        } else {
+            $this->results = $results;
+        }
     }
     
-    public function searchBox($context, $fromPage){
-		
-	$template = $context->createTemplate();
-	$template->searchForm = $this->createComponentSearchForm('searchForm', $context, $fromPage);
-	$template->setFile('../app/templates/search-module/Search/boxes/search.latte');
+    public function searchBox($context, $fromPage)
+    {
+    	$template = $context->createTemplate();
+    	$template->searchForm = $this->createComponentSearchForm('searchForm', $context, $fromPage);
+    	$template->setFile('../app/templates/search-module/Search/boxes/search.latte');
 
-	return $template;
+    	return $template;
     }
 }
